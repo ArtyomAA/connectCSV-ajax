6e3c4ce3fa21c81bfa059822794395b0bfcfcc5f
ArtyomAA <62956757+ArtyomAA@users.noreply.github.com>
2020-08-19 10:54:21 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<form method="POST" enctype="multipart/form-data" id="fileUploadForm"><input class="inn" name="search" type="text" placeholder="Введите ИНН организации" /> <button type="submit">Найти</button></form>
+<p class="peremen"></p>
+<div id="result">
+<table>
+            <thead>
+              <tr>
+                <th>Срок действия</th>
+                <th>Название</th>
+                <th>ИНН</th>
+                <th>Юридический адрес</th>
+                <th>Выдан на имя</th>
+                <th>Регистрационный номер</th>
+                <th>Область сертификации</th>
+                <th>Решение экспертной комиссии</th>
+              </tr>
+            </thead>
+            <tbody class='skritie'></tbody> </table>
+</div>
+{source}<?php include '/var/www/u1124859/data/www/unionstandard.ru/parsim.php';  ?>{/source}
+</div>
